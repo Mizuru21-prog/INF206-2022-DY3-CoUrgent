1d54f724293421190a5c369baa6c4ff8f36f0756
naufalalkhalls <naufalalkhalis@gmail.com>
2022-04-18 16:16:01 +0700

    layouts, produk, struk

---
+++ resources/views/Struk.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main')
+    @section('container')
+    
+    <h1>halaman struct</h1>
+    @endsection
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/Struk', function () {
+    return view('Struk',[
+        "title" => "Struk"
+    ]);
 });
+Route::get('/Produk', function () {
+    return view('Produk', [
+        "title" =>'Produk'
+    ]);
+});
+
+
