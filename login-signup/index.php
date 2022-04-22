c0d121652c5144c426be370cf5d6994532507cc6
DitaAnatasya <81795819+DitaAnatasya@users.noreply.github.com>
2022-04-22 19:45:16 +0700

    Update index.php

--- login-signup/index.php
+++ login-signup/index.php
@@ -1,4 +1,27 @@
-<?php 
+<?php
+/* nama server kita */
+$servername = "localhost";
+
+/* nama database kita */
+$database = "db_pembeli"; 
+
+/* nama user yang terdaftar pada database (default: root) */
+$username = "root";
+
+/* password yang terdaftar pada database (default : "") */ 
+$password = ""; 
+
+/* membuat koneksi */
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+/* mengecek koneksi */
+if (!$conn) {
+    die("Maaf koneksi anda gagal : " . mysqli_connect_error());
+}else{
+   echo "<h1>Yes! Koneksi Berhasil..</h1>";
+}
+
+
     if(isset($_POST["tujuan"])){
 
         $tujuan = $_POST["tujuan"];
@@ -23,4 +46,23 @@
             
         }
     }   
+
+    $username = $_POST["username"];
+$password = $_POST["password"];
+$email = $_POST["email"];
+
+$query_sql = "INSERT INTO tbl_pendaftaran (username, password, email) 
+                                    VALUES ('$username', '$password', '$email')";
+
+if (mysqli_query($conn, $query_sql)) {
+      echo "<h1>Username $username berhasil terdaftar</h1>
+            <a href='pages/login.php'>Kembali Login</h1>
+         ";
+} else {
+      echo "Pendaftaran Gagal : " . mysqli_error($conn);
+}
+?>
+
+/* menutup koneksi */
+mysqli_close($conn);
 ?>
