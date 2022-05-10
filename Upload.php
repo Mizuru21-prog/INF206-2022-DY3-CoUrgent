491e64076d3205fe80b79326c2c0ddd500243ad6
naufalalkhalls <naufalalkhalis@gmail.com>
2022-05-10 23:50:01 +0700

    naufal s2 minor

--- Upload.php
+++ Upload.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>CoUrgent | Keranjang</title>
+    <title>CoUrgent | Upload</title>
     <link rel="stylesheet" href="CSS/style.css">
     <link rel="stylesheet" href="CSS/produk.css">
     <!-- font awesome--> 
@@ -87,7 +87,7 @@
 
     <div id="copyright">
         <div class="wrapper">
-            &copy; 2022. <b>CoUrgents</b> All Right Reserve
+            &copy; 2022. <b>CoUrgent</b> All Right Reserve
         </div>
     </div>
 
