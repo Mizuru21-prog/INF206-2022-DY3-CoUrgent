4d824bb9c815260f587fb47f6164f02a2794a742
naufalalkhalls <naufalalkhalis@gmail.com>
2022-05-14 10:48:33 +0700

    Perbaikan mayor FE

--- Upload.php
+++ Upload.php
@@ -1,21 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>CoUrgent | Upload</title>
+    <title>CoUrgent | Produk</title>
     <link rel="stylesheet" href="CSS/style.css">
-    <link rel="stylesheet" href="CSS/produk.css">
-    <!-- font awesome--> 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.css" />
-  
-    <!-- bootstrap cdn--> 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-</head>
-<body>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <style>
+        .mx-auto {
+            width: 800px
+        }
 
+        .card {
+            margin-top: 10px;
+        }
+    </style>
+</head>
+
+<body>
     <nav>
         <div class="wrapper">
             <div class="logo"><a href="">CoUrgent</a></div>
@@ -23,44 +27,55 @@
                 <ul>
                     <li><a href="Home.php">Home</a></li>
                     <li><a href="Produk.php">Produk</a></li>
-                    <li><a href="Upload.php">Upload</a></li>  
+                    <li><a href="Upload.php">Upload</a></li>
                     <li><a href="Keranjang.php">Keranjang</a></li>
                     <li><a href="" class="tbl-hijau">Log out</a></li>
-                                    
+
                 </ul>
             </div>
         </div>
     </nav>
 
-    <link rel="stylesheet" href="CSS/style.css">
-    <h1 class="up">Upload Produk</h1>
+    <div class="mx-auto">
+        <!-- untuk memasukkan data -->
+        <div class="card">
+            <div class="card-header">
+                Upload data produk
+            </div>
+            <div class="card-body">
+                <form action="" method="POST">
+                    <div class="mb-3 row">
+                        <label for="Nama" class="col-sm-2 col-form-label">Nama</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="Nama" name="Nama" value="<?php echo $nim ?>">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="Keterangan" class="col-sm-2 col-form-label">Keterangan</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="Keterangan" name="Keterangan" value="<?php echo $nama ?>">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="Harga" class="col-sm-2 col-form-label">Harga</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="Harga" name="Harga" value="<?php echo $alamat ?>">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="formFile" class="form-label">Upload Gambar Produk</label>
+                        <input class="form-control" type="file" id="formFile">
+                    </div>
+            <div class="col-12">
+                <input type="submit" name="simpan" value="Simpan Data" class="btn btn-primary" />
+            </div>
+            </form>
+        </div>
+    </div>
+    </div>
 
-
-<div class="col-lg-8">
-    <form>
-    <div class="wrap">
-    <div class="mb-3">
-      <div class="mb-3">
-        <label for="namaProduk" class="form-label">Nama Produk</label>
-        <input type="text" class="form-control" id="exampleNamaProduk">
-      </div>
-      <div class="mb-3">
-        <label for="hargaSatuan" class="form-label">Harga Satuan</label>
-        <input type="int" class="form-control" id="exampleHargaSatuan">
-      </div>
-      <div class="mb-3">
-        <label for="stok" class="form-label">Stok</label>
-        <input type="int" class="form-control" id="exampleStok">
-      </div>
-      <div class="mb-3">
-        <label for="formFile" class="form-label">Upload Gambar Produk</label>
-        <input class="form-control" type="file" id="formFile">
-      </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
-      </div>
-    </form>
-</div>
-<div id="contact">
+    </div>
+    <div id="contact">
         <div class="wrapper">
             <div class="footer">
                 <div class="footer-section">
@@ -84,16 +99,13 @@
         </div>
     </div>
 
-
     <div id="copyright">
         <div class="wrapper">
             &copy; 2022. <b>CoUrgent</b> All Right Reserve
         </div>
     </div>
 
-
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    
 </body>
+
 </html>
