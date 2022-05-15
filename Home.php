035a123f082428bbbbb6f9682fc5367df8635cb4
Sima Maulina <simamauli@mhs.unsyiah.ac.id>
2022-05-15 12:20:13 +0700

    hapus obrolan home

--- Home.php
+++ Home.php
@@ -35,18 +35,6 @@
 <div class="container">
     <div class="row text-center py-5">
       <div class="col-md-3 col-sm-6 my-3 my-md-0">
-          <form action="home.blade.php" method="post">
-              <div class="card shadow">
-                  <div>
-                      <img src="./img/chat.png" alt="Image1" class="img-fluid card-img-top">
-                  </div>
-                  <div class="card-body">
-                      <button type="submit" class="btn btn-warning my-3"name="add">Obrolan</button>
-                  </div>
-              </div>            
-          </form>
-      </div>
-      <div class="col-md-3 col-sm-6 my-3 my-md-0">
       <form action="home.blade.php" method="post">
               <div class="card shadow">
                   <div>
