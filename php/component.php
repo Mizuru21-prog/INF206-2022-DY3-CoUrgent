1fd4ea3a2982d85bdffde9a58255c0f7c2af4a48
DitaAnatasya <81795819+DitaAnatasya@users.noreply.github.com>
2022-05-23 08:30:49 +0700

    Update component.php

--- php/component.php
+++ php/component.php
@@ -1,16 +1,16 @@
 <?php
 
-function component($productname, $productprice, $productimg, $productid){
+function component($product_name, $product_price, $product_image, $id){
     $element = "
     
     <div class=\"col-md-3 col-sm-6 my-3 my-md-0\">
-                <form action=\"Produk.php\" method=\"post\">
+                <form action=\"buyerProduk.php\" method=\"post\">
                     <div class=\"card shadow\">
                         <div>
-                            <img src=\"$productimg\" alt=\"Image1\" class=\"img-fluid card-img-top\">
+                            <img src=\"$product_image\" alt=\"Image1\" class=\"img-fluid card-img-top\">
                         </div>
                         <div class=\"card-body\">
-                            <h5 class=\"card-title\">$productname</h5>
+                            <h5 class=\"card-title\">$product_name</h5>
                             <h6>
                                 <i class=\"fas fa-star\"></i>
                                 <i class=\"fas fa-star\"></i>
@@ -22,12 +22,12 @@ function component($productname, $productprice, $productimg, $productid){
                                 Keterangan Produk yang ingin di jual
                             </p>
                             <h5>
-                                <small><s class=\"text-secondary\">Rp519</s></small>
-                                <span class=\"price\">Rp$productprice</span>
+                                <small><s class=\"text-secondary\"></s></small>
+                                <span class=\"price\">Rp$product_price</span>
                             </h5>
 
                             <button type=\"submit\" class=\"btn btn-warning my-3\" name=\"add\">+ Keranjang <i class=\"fas fa-shopping-cart\"></i></button>
-                             <input type='hidden' name='product_id' value='$productid'>
+                             <input type='hidden' name='product_id' value='$id'>
                         </div>
                     </div>
                 </form>
@@ -39,7 +39,7 @@ function component($productname, $productprice, $productimg, $productid){
 function cartElement($productimg, $productname, $productprice, $productid){
     $element = "
     
-    <form action=\"Keranjang.php?action=remove&id=$productid\" method=\"post\" class=\"cart-items\">
+    <form action=\"buyerKeranjang.php?action=remove&id=$productid\" method=\"post\" class=\"cart-items\">
                     <div class=\"border rounded\">
                         <div class=\"row bg-white\">
                             <div class=\"col-md-3 pl-0\">
@@ -47,8 +47,8 @@ function cartElement($productimg, $productname, $productprice, $productid){
                             </div>
                             <div class=\"col-md-6\">
                                 <h5 class=\"pt-2\">$productname</h5>
-                                <small class=\"text-secondary\">Penjual: CoUrgent</small>
-                                <h5 class=\"pt-2\">$$productprice</h5>
+                                <small class=\"text-secondary\">Apotek: CoUrgent</small>
+                                <h5 class=\"pt-2\">Rp$productprice</h5>
                                 <button type=\"submit\" class=\"btn btn-warning\">Simpan untuk nanti</button>
                                 <button type=\"submit\" class=\"btn btn-danger mx-2\" name=\"remove\">Hapus</button>
                             </div>
@@ -59,20 +59,3 @@ function cartElement($productimg, $productname, $productprice, $productid){
     ";
     echo  $element;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
