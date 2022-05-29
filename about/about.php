3e6b7fddcd94a2269c8c75f2fcc87510ad3aef61
DitaAnatasya <81795819+DitaAnatasya@users.noreply.github.com>
2022-05-29 23:26:46 +0700

    halaman about us

---
+++ about/about.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>CoUrgnet | About Us</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body class="bg-light">
+ 
+  <!-- navbar start-->
+  <nav class="navbar navbar-light fixed-top bg-light shadow-sm">
+    <div class="container-lg shadow-sm">
+      <a class="navbar-brand text-info fw-bold fs-4" href="#">DY3 - CoUrgent</a>
+    </div>
+  </nav>
+
+  <!-- navbar end-->
+
+  <!-- Freelancae available sections start-->
+<section class="freelance-available py-5 bg-info">
+  <div class="container-lg py-4">
+      <div class="row justify-content-center">
+          <div class="col-lg-8 text-center">
+            <a class="navbar-brand text-white fw-bold fs-1" >Apa itu CoUrgent</a> <br> <br>
+              <p class="fs-4 text-white mb-4">CoUrgent adalah aplikasi berbasis website yang menyediakan informasi tempat yang menjual berbagai
+                produk kesahatan di era pandemi dimana proses pengambilan produk dilakukan secara terjadwal.
+              </p>
+                <a href="../landing.php" class="btn btn-outline-light">Gabung Sekarang</a>
+          </div>
+      </div>
+  </div>
+</section>
+  <!-- Freelancae available sections end-->
+
+  <!-- Portfolio sections Start-->
+  <section class="portfolio py-5" id="portfolio">
+    <div class="container-lg py-4">
+      <div class="row justify-content-center">
+        <div class="col-lg-8">
+          <div class="section-title text-center">
+            <a class="navbar-brand text-info fw-bold fs-1" >CoUrgent Dev </a> <br> <br>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+
+        <div class="col-md-3">
+          <div class="portfolio-item">
+            <img src="img/ans.jpeg" class="w-100 img-thumbnail" alt="portfolio-item">
+            <h3 class="text-capitalize fs-5 my-2 text-center">Anas Naufal Al-Kiram</h3>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="portfolio-item">
+            <img src="img/dit.jpeg" class="w-100 img-thumbnail" alt="portfolio-item">
+            <h3 class="text-capitalize fs-5 my-2 text-center">Dita Anatasya</h3>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="portfolio-item">
+            <img src="img/n.jpeg" class="w-100 img-thumbnail" alt="portfolio-item">
+            <h3 class="text-capitalize fs-5 my-2 text-center">Naufal Alkhalis</h3>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="portfolio-item">
+            <img src="img/sm.jpeg" class="w-100px h-100px img-thumbnail" alt="portfolio-item">
+            <h3 class="text-capitalize fs-5 my-2 text-center">Sima Maulina</h3>
+          </div>
+        </div>
+      </div>
+        
+  <!-- Portfolio sections End-->
+  <!-- Footer sections start-->
+  <footer class="footer border-top py-4">
+      <div class="container-lg">
+          <div class="row">
+              <div class="col-lg-12">
+                  <p class="m-0 text-center text-muted">&copy; 2022 CoUrgent Group</p>
+              </div>
+          </div>
+      </div>
+  </footer>
+  <!-- Footer sections end-->
+</body>
+</html>
