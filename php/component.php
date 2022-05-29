30af988da5710a4540d965fae3351810edad3f1f
DitaAnatasya <81795819+DitaAnatasya@users.noreply.github.com>
2022-05-29 22:54:30 +0700

    Update component.php

--- php/component.php
+++ php/component.php
@@ -1,16 +1,18 @@
 <?php
 
-function component($product_name, $product_price, $product_image, $id){
+function component($product_name, $apotek_name, $product_price, $product_image, $id, $ket, $alamat, $kode, $pukul, $tanggal){
     $element = "
     
     <div class=\"col-md-3 col-sm-6 my-3 my-md-0\">
                 <form action=\"buyerProduk.php\" method=\"post\">
                     <div class=\"card shadow\">
                         <div>
-                            <img src=\"$product_image\" alt=\"Image1\" class=\"img-fluid card-img-top\">
+                            <img src=\"$product_image\" alt=\"Image1\" class=\"img-fluid card-img-top\" style = \"height : 13rem\">
                         </div>
                         <div class=\"card-body\">
-                            <h5 class=\"card-title\">$product_name</h5>
+                            <h5 class=\"card-title\">$product_name <br>
+                            <span class=\"price\">Rp$product_price</span> <br> <br>
+                            </h5>
                             <h6>
                                 <i class=\"fas fa-star\"></i>
                                 <i class=\"fas fa-star\"></i>
@@ -18,13 +20,18 @@ function component($product_name, $product_price, $product_image, $id){
                                 <i class=\"fas fa-star\"></i>
                                 <i class=\"far fa-star\"></i>
                             </h6>
-                            <p class=\"card-text\">
-                                Keterangan Produk yang ingin di jual
+                            <p class=\"card-text\"> 
+                               $ket
                             </p>
-                            <h5>
-                                <small><s class=\"text-secondary\"></s></small>
-                                <span class=\"price\">Rp$product_price</span>
-                            </h5>
+                            <p class=\"card-text\">
+                               Alamat: $alamat
+                            </p>
+                            <p class=\"card-text\">
+                                Kode Apotek: $kode
+                            </p>
+                            <h6>
+                                <small><s class=\"text-secondary\">Jadwal: $pukul, $tanggal</s></small> <br>
+                            </h6>
 
                             <button type=\"submit\" class=\"btn btn-warning my-3\" name=\"add\">+ Keranjang <i class=\"fas fa-shopping-cart\"></i></button>
                              <input type='hidden' name='product_id' value='$id'>
@@ -36,19 +43,22 @@ function component($product_name, $product_price, $product_image, $id){
     echo $element;
 }
 
-function cartElement($productimg, $productname, $productprice, $productid){
+function cartElement($product_image, $product_name, $apotek_name, $product_price, $id, $ket, $alamat, $kode, $pukul, $tanggal){
     $element = "
     
-    <form action=\"buyerKeranjang.php?action=remove&id=$productid\" method=\"post\" class=\"cart-items\">
+    <form action=\"buyerKeranjang.php?action=remove&id=$id\" method=\"post\" class=\"cart-items\">
                     <div class=\"border rounded\">
                         <div class=\"row bg-white\">
                             <div class=\"col-md-3 pl-0\">
-                                <img src=$productimg alt=\"Image1\" class=\"img-fluid\">
+                                <img src=$product_image alt=\"Image1\" class=\"img-fluid\">
                             </div>
                             <div class=\"col-md-6\">
-                                <h5 class=\"pt-2\">$productname</h5>
-                                <small class=\"text-secondary\">Apotek: CoUrgent</small>
-                                <h5 class=\"pt-2\">Rp$productprice</h5>
+                                <h6 class=\"pt-2\">$product_name</h6>
+                                <h6 class=\"pt-2\">Rp$product_price</h6>
+                                <small class=\"text-secondary\">Apotek: $apotek_name</small> <br>
+                                <small class=\"text-secondary\">Alamat: $alamat</small> <br>
+                                <small class=\"text-secondary\">Deskripsi: $ket</small> <br>
+                                <h6 class=\"pt-2\">$pukul, $tanggal</h6>
                                 <button type=\"submit\" class=\"btn btn-warning\">Simpan untuk nanti</button>
                                 <button type=\"submit\" class=\"btn btn-danger mx-2\" name=\"remove\">Hapus</button>
                             </div>
